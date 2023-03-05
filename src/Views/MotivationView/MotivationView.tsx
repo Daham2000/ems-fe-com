d012fe18ecceec1b3afc4f6ad075510b22db7ce8
Daham2000 <daham.akalanka@htown-tech.com>
2023-03-05 08:39:04 +0530

    Motivational requests UI

--- src/Views/MotivationView/MotivationView.tsx
+++ src/Views/MotivationView/MotivationView.tsx
@@ -2,6 +2,21 @@ import DashboardTopBar from "../Shared/dashboard-top-bar";
 import { Form } from "react-bootstrap";
 import ButtonComponent from "../Shared/button-component";
 
+const EmployeeRequestCard = (props: any) => {
+    return <div className="d-flex flex-column justify-content-start align-item-start bg-color-white"
+        style={{ width: "auto", height: "auto", marginTop: "10px", marginBottom: "5px", padding: "15px", borderRadius: "10px" }}>
+        <div className="announcement-title-normal">{props.name}</div>
+        <div className="announcement-title-normal" style={{ fontSize: "12px" }}>{"EMP ID: " + props.empID}</div>
+        <div className="announcement-title-normal" style={{ fontSize: "12px" }}>{"EMP Job title: " + props.jobTitle}</div>
+        <div className="announcement-des-normal">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea repellat, reiciendis
+            quo corporis officiis, voluptatem magni perspiciatis debitis est
+            dignissimos quos praesentium suscipit velit accusamus dolor
+            exercitationem impedit cum atque. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Necessitatibus, omnis itaque non fuga quas est reprehenderit aliquam autem recusandae
+            voluptatibus distinctio porro quasi dignissimos aut. Molestiae optio voluptatem cumque odio.</div>
+    </div>
+}
+
 const MotivationView = () => {
     return <div className="d-flex flex-column justify-content-start align-item-start">
         <DashboardTopBar
@@ -9,6 +24,13 @@ const MotivationView = () => {
             profileUrl="https://nadiazheng.com/wp-content/uploads/2015/12/Montreal-personal-branding-linkedin-profile-professional-headshot-by-nadia-zheng-800x1000.jpg"
             wish={1} />
         <div className="sub-topic-font " style={{ marginTop: "15px" }}>{"Motivation Page"}</div>
+
+        <div className="sub-topic-font" style={{ marginTop: "15px", fontSize: "12px" }}>{"Employee requests"}</div>
+        <div className="d-flex flex-column justify-content-start align-item-start">
+            <EmployeeRequestCard name="John Cick" empID="E0001" jobTitle="Software developer" />
+            <EmployeeRequestCard name="John Cick" empID="E0001" jobTitle="Software developer" />
+            <EmployeeRequestCard name="John Cick" empID="E0001" jobTitle="Software developer" />
+        </div>
 
         <div className="d-flex flex-row bg-color-white justify-content-between"
             style={{ borderRadius: "10px", width: "auto", marginTop: "10px", paddingLeft: "10px", paddingRight: "10px", paddingTop: "10px", paddingBottom: "10px", marginBottom: "15px" }}>
@@ -75,8 +97,8 @@ const MotivationView = () => {
                     }
                 </div>
                 <div style={{ height: "20px" }} />
-                <MotivationSelfCard/>
-                <MotivationSelfCard/>
+                <MotivationSelfCard />
+                <MotivationSelfCard />
             </div>
         </div>
 
@@ -93,18 +115,18 @@ const PictureCard = () => {
 }
 
 const MotivationSelfCard = () => {
-    return <div className="d-flex flex-row" style={{marginBottom: "10px"}}>
-    <img height="70px" width="100px" style={{ borderRadius: "10px" }}
-        src={"https://snacknation.com/wp-content/uploads/2021/06/shutterstock_1386599774-scaled-e1624375499882.jpg"} />
-    <div className="d-flex flex-column justify-content-start align-item-start" style={{ marginLeft: "10px" }}>
-        <div className="sub-topic-font" style={{ marginTop: "5px", marginBottom: "1px", fontSize: "13px" }}>{"Work-Life Balancing"}</div>
-        <div className="announcement-des-normal" style={{ marginTop: "1px", paddingTop: "0", width: "400px" }}>
-            {
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has "
-            }
+    return <div className="d-flex flex-row" style={{ marginBottom: "10px" }}>
+        <img height="70px" width="100px" style={{ borderRadius: "10px" }}
+            src={"https://snacknation.com/wp-content/uploads/2021/06/shutterstock_1386599774-scaled-e1624375499882.jpg"} />
+        <div className="d-flex flex-column justify-content-start align-item-start" style={{ marginLeft: "10px" }}>
+            <div className="sub-topic-font" style={{ marginTop: "5px", marginBottom: "1px", fontSize: "13px" }}>{"Work-Life Balancing"}</div>
+            <div className="announcement-des-normal" style={{ marginTop: "1px", paddingTop: "0", width: "400px" }}>
+                {
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has "
+                }
+            </div>
         </div>
-    </div>
-</div>;
+    </div>;
 }
 
 export default MotivationView;
